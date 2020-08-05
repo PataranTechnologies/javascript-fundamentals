a09fe93dbd66f002ff237d00428bb1a52e05aba1
shristi54 <shristi1350@gmail.com>
2020-08-05 10:55:38 +0530

    first commit

---
+++ operators.js
@@ -0,0 +1,8 @@
+//assignment operator//
+var x = 10;
+x += 5;
+console.log(x);
+
+var y = 10;
+y /= 5;
+console.log(y);
---
+++ string.js
@@ -0,0 +1,13 @@
+//string concatenation//
+var txt1 = "What a very" + " nice day.";
+
+console.log(txt1);
+
+//adding string by using += //
+var txt3 = "What a very ";
+txt3 += "nice day";
+console.log(txt3);
+
+//blackslash before quote accepts quote as quote//
+var y = 'We are the so-called "Vikings" from the north.';
+console.log(y);
