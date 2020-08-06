b44680ee9ffcf1e34c6fe47e4e0a6413b6e69a82
shristi54 <shristi1350@gmail.com>
2020-08-06 17:29:48 +0530

    first commit

--- objects.js
+++ objects.js
@@ -17,3 +17,21 @@ var testObj = {
 var entreeValue = testObj["an entree"];
 var drinkVlue = testObj["the drink"];
 console.log(entreeValue, drinkVlue);
+
+//checking properties of object//
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh",
+};
+function checkObj(checkProp) {
+  if (myObj.hasOwnProperty(checkProp)) {
+    return myObj[checkProp];
+  } else {
+    return "Not Found";
+  }
+}
+
+console.log(checkObj("gift"));
+
+//JSON.parse(Json.stringify())-- fancy way in js to copy the object//
